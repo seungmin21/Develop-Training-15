b392de338dbf76431571ba0144009fb14a3a12f0
ohseungmin <ohmin2886@gmail.com>
2023-12-18 18:43:51 +0900

    async부터 useState까지 이어보기

---
+++ object.js
@@ -0,0 +1,27 @@
+import React, { useEffect, useState } from 'react';
+
+const fetchProducts = async () => {
+  return new Promise(resolve => {
+    setTimeout(() => {
+      resolve([
+        { id: 1, title: '김치', price: '5000원' },
+        { id: 2, title: '라면', price: '3000원' },
+        { id: 3, title: '삼겹살', price: '15000원' },
+      ]);
+    }, 500);
+  });
+}
+
+const Carousel = ({ fetchProducts }) => {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    const loadProducts = async () => {
+      const data = await fetchProducts();
+      setProducts(data);
+    };
+    loadProducts();
+  }, [fetchProducts]);
+}
+
+console.log(Carousel)
